3abe05e4113f8bd8584e0398e2a7ad0728858645
Diego Llacsahuanga <llacsahuanga.buques@gmail.com>
2025-07-27 11:40:43 -0500

    fix: swiper's css import

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,16 @@
 /// <reference types="vite/client" />
+
+declare module '*.css' {
+  const css: string;
+  export default css;
+}
+
+declare module 'swiper/css' {
+  const css: string;
+  export default css;
+}
+
+declare module 'swiper/css/*' {
+  const css: string;
+  export default css;
+}
